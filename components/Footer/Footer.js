05979ebc7336a5b0521d29965a0b23f0a640fc65
Naufal Akbar <kbrnugroho@gmail.com>
2021-12-27 13:00:46 +0700

    change footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Copyright  from './Copyright'
 import SocialMediaLinks  from './SocialMediaLinks'
+import styles from '../../styles/Footer.module.css'
+import { FaExternalLinkAlt } from 'react-icons/fa'
 
 const Footer = () => {
     return (
         <footer>
             <div>
                 <div style={{ display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center", color: "white" }}>
-                    <h5 style={{ marginRight: "30px" }}><a href="https://naufalakbar.me/" target="_blank" rel="noreferrer">Naufal Akbar Nugroho</a></h5>
+                    <h5 style={{ marginRight: "30px" }}><a href="https://github.com/nuflakbrr/next-news" target="_blank" rel="noreferrer" className={styles.source}>Source Code <FaExternalLinkAlt style={{ marginLeft: "5px" }} /></a></h5>
                     <SocialMediaLinks />
                 </div>
                 <Copyright />
